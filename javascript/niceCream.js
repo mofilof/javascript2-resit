45e397caa5d6eb08222db28e17bf73c098a2cddc
mofilof <44873229+mofilof@users.noreply.github.com>
2020-06-24 00:31:24 +0200

    Main files resit

---
+++ javascript/niceCream.js
@@ -0,0 +1,4 @@
+//If x is clicked sessionstorage xtaste/flavour and x$ price
+
+// $ needs to be connected to an end value and added togheter with Math()
+//Need to be refreshed whenever something is clicked.
